5210d44bce10072b64bcba80b955c25ec168f068
Steve Scaffidi <sscaffidi@cloudera.com>
2024-09-05 13:39:24 -0400

    Added all code from tutorial

--- internal/controller/pod_controller.go
+++ internal/controller/pod_controller.go
@@ -20,10 +20,11 @@ import (
 	"context"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	logger "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // PodReconciler reconciles a Pod object
@@ -31,6 +32,11 @@ type PodReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+const (
+	addPodNameLabelAnnotation = "padok.fr/add-pod-name-label"
+	podNameLabel              = "padok.fr/pod-name"
+)
 
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
@@ -46,9 +52,67 @@ type PodReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := logger.FromContext(ctx)
 
-	// TODO(user): your logic here
+	/*
+	   Step 0: Fetch the Pod from the Kubernetes API.
+	*/
+
+	var pod corev1.Pod
+	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
+		if apierrors.IsNotFound(err) {
+			// we'll ignore not-found errors, since we can get them on deleted requests.
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "unable to fetch Pod")
+		return ctrl.Result{}, err
+	}
+
+	/*
+	   Step 1: Add or remove the label.
+	*/
+
+	labelShouldBePresent := pod.Annotations[addPodNameLabelAnnotation] == "true"
+	labelIsPresent := pod.Labels[podNameLabel] == pod.Name
+
+	if labelShouldBePresent == labelIsPresent {
+		// The desired state and actual state of the Pod are the same.
+		// No further action is required by the operator at this moment.
+		log.Info("no update required")
+		return ctrl.Result{}, nil
+	}
+
+	if labelShouldBePresent {
+		// If the label should be set but is not, set it.
+		if pod.Labels == nil {
+			pod.Labels = make(map[string]string)
+		}
+		pod.Labels[podNameLabel] = pod.Name
+		log.Info("adding label")
+	} else {
+		// If the label should not be set but is, remove it.
+		delete(pod.Labels, podNameLabel)
+		log.Info("removing label")
+	}
+
+	/*
+	   Step 2: Update the Pod in the Kubernetes API.
+	*/
+
+	if err := r.Update(ctx, &pod); err != nil {
+		if apierrors.IsConflict(err) {
+			// The Pod has been updated since we read it.
+			// Requeue the Pod to try to reconciliate again.
+			return ctrl.Result{Requeue: true}, nil
+		}
+		if apierrors.IsNotFound(err) {
+			// The Pod has been deleted since we read it.
+			// Requeue the Pod to try to reconciliate again.
+			return ctrl.Result{Requeue: true}, nil
+		}
+		log.Error(err, "unable to update Pod")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
