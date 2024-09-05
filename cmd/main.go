24cff177cc321ddf1e781904cdeb13cba96932e0
Steve Scaffidi <sscaffidi@cloudera.com>
2024-09-05 11:13:33 -0400

    generated controller skeleton

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.infra.cloudera.com/sscaffidi/istio-proxy-update-controller/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -118,6 +120,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.PodReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Pod")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
