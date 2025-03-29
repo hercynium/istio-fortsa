3add152d97604ef0e5dcdfa518f5870afde1605e
Steve Scaffidi <sscaffidi@cloudera.com>
2025-03-29 14:21:41 -0400

    removed pod controller - no longer needed

--- cmd/main.go
+++ cmd/main.go
@@ -156,15 +156,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
 		os.Exit(1)
 	}
-	if err = (&controller.PodReconciler{
-		Client:     mgr.GetClient(),
-		Scheme:     mgr.GetScheme(),
-		Config:     cfg,
-		KubeClient: kubeClient,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Pod")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
