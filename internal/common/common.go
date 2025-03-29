3eb0e613ae4e1c5ce2b19f7871e80b9e79e0614d
Steve Scaffidi <sscaffidi@cloudera.com>
2025-03-29 14:26:14 -0400

    removed more dead code

--- internal/common/common.go
+++ internal/common/common.go
@@ -6,7 +6,4 @@ const (
 
 	// k8s object label for istio revision tag
 	IstioTagLabel = "istio.io/tag"
-
-	// add this label to pods found connected to an outdated istiod
-	PodOutdatedLabel = "fortsa.scaffidi.net/outdatedAt"
 )
